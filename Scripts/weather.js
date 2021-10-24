1a1e3f95faca269921ef00f837bb61d730e5842b
Hannan Afridi <83342729+afridihannan@users.noreply.github.com>
2021-10-24 10:02:23 +0530

    add search js

--- Scripts/weather.js
+++ Scripts/weather.js
@@ -5,8 +5,16 @@ let tempa=document.getElementById('tamp');
 
 const tempStatus="clouds";
 
+var inpu=document.getElementById('inpu');
+var cityname;
+var search=document.querySelector('#search');
+search.addEventListener("click",()=>{
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+document.querySelector('input').value+'&appid=cd53e372a71a5447e5178d1bfdc17ff0').then(response=>response.json()).then(data=>showinfo(data)).catch(err=>alert("City name not valid"));
+    document.querySelector('input').value="";
+})
+console.log(cityname);
 var info;
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Guwahati&appid=cd53e372a71a5447e5178d1bfdc17ff0').then(response=>response.json()).then(data=>showinfo(data)).catch(err=>alert("City name not valid"));
+
 
 console.log(info);
 
